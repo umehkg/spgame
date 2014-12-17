9f2c2c9299ed7b7a08be539b5fe04ea2994ae4bd
umehkg <umehkg@users.noreply.github.com>
2014-12-18 06:17:39 +0800

    Create CheckCheckSum.cpp
    initial commit

---
+++ src/iosocketdll/classes/CGenericMessage/CheckCheckSum.cpp
@@ -0,0 +1,7 @@
+bool __thiscall CGenericMessage::CheckCheckSum() //+2440
+{
+	DecryptBody( (char *)(this+4) ,*(LPDWORD)this - 4);
+	if ( this->MakeDigest() == *(LPDWORD)(this+0xC) )
+		return true;
+	return false;
+}
