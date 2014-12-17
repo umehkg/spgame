69ade4663af778fb567809df589f2e31942b0a3c
umehkg <umehkg@users.noreply.github.com>
2014-12-17 09:29:45 +0800

    Update EncryptMsg.cpp

--- src/iosocketdll/classes/CGenericMessage/EncryptMsg.cpp
+++ src/iosocketdll/classes/CGenericMessage/EncryptMsg.cpp
@@ -1,5 +1,5 @@
 void __thiscall CGenericMessage::EncryptMsg()
 {
 	*(LPDWORD)(DWORD)this+0xC = this->MakeDigest();
-	Encrypt((char *)(DWORD)this+0x4, (*(LPDWORD)this)-4);
+	EncryptBody((char *)(DWORD)this+0x4, (*(LPDWORD)this)-4);
 }
