945f80264ff15e7f2d1a25ca790bfa159d85524d
umehkg <umehkg@users.noreply.github.com>
2014-12-18 06:35:36 +0800

    Update CheckCheckSum.cpp
    fixed bug

--- src/iosocketdll/classes/CGenericMessage/CheckCheckSum.cpp
+++ src/iosocketdll/classes/CGenericMessage/CheckCheckSum.cpp
@@ -2,6 +2,6 @@ bool __thiscall CGenericMessage::CheckCheckSum() //+2440
 {
 	DecryptBody( (char *)(this+4) ,*(LPDWORD)this - 4);
 	if ( this->MakeDigest() == *(LPDWORD)(this+0xC) )
-		return true;
-	return false;
+		return false;
+	return true;
 }
