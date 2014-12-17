3efabba7f094d8a5b887e998d5520b184efefc75
umehkg <umehkg@users.noreply.github.com>
2014-12-18 05:39:07 +0800

    Update MakeDigest.cpp
    cleaned up code

--- src/iosocketdll/classes/CGenericMessage/MakeDigest.cpp
+++ src/iosocketdll/classes/CGenericMessage/MakeDigest.cpp
@@ -1,10 +1,7 @@
+#include "md5.h"
 DWORD CGenericMessage::MakeDigest( void )
 {
-	DWORD msgDigest = *(LPDWORD)(DWORD)this+0xC;
-	*(LPDWORD)this+0xC = 0; //+23CF
-	MD5 *md5 = new MD5(); //see MD5 library, MD5::init() called by constructor
-	DigestMsg2((char *)md5, (char *)this, *(LPDWORD)this); //+23E4 call to +44E0
-	md5->transform((char *)(DWORD)md5-0x10, (char *)md5); //result stored in param1, input is param2
-	char *var1 = (char *)(DWORD)md5-0x10;
-	return *(LPDWORD)(DWORD)var1 ^ *(LPDWORD)(DWORD)var1+4 ^ *(LPDWORD)(DWORD)var1+8 ^ *(LPDWORD)(DWORD)var1+0xC;
+	char buf[0x11];
+	md5_raw(buf, this, *(LPDWORD)this);
+	return *(LPDWORD)buf ^ *(LPDWORD)(buf+4) ^ *(LPDWORD)(buf+8) ^ *(LPDWORD)(buf+0xC);
 }
