f93e9b683d3ae1f07cc6470c0bf65791fa4727bf
umehkg <umehkg@users.noreply.github.com>
2014-12-17 10:10:04 +0800

    Update 44E0_DigestMsg2.cpp

--- src/iosocketdll/orphaned_functions/44E0_DigestMsg2.cpp
+++ src/iosocketdll/orphaned_functions/44E0_DigestMsg2.cpp
@@ -1,19 +1,40 @@
-void DigestMsg2(char *param1, int packetSize)
+void DigestMsg2(char *md5, char *packet, int packetSize)
 {
 //Ref: iosocketdll.dll+44E0
-	//param1 same as in DigestMsg
-	DWORD eax = *(DWORD *)param1+0x10;
-	ecx = eax + packetSize*8;
-	*(DWORD *)param1+0x10 = ecx;
-	if (ecx < eax)
-		*(DWORD *)(param1+0x14)++;
+	char *ebx = md5; //+44E1
+	DWORD eax = *(LPDWORD)(DWORD)param1+0x10; //+44E5
+	ecx = eax + packetSize*8; //+44ED
+	*(LPDWORD)(DWORD)md5+0x10 = ecx; //+44F4
+	if (ecx < eax) //+44F0
+		( *(LPDWORD)(DWORD)md5+0x14)++;
+	esi = *(DWORD *)(DWORD)md5+0x14;
 	eax >>= 3;
 	eax &= 0x3F;
-	*(DWORD *)param1+0x14 += packetSize>>0x1D;
+	*(DWORD *)param1+0x14 += packetSize>>0x1D; //+450C
 	if (ecx != eax)
 	{
 		char *esi = packet;
-		
+		ecx = 0x40;
+		ecx -= eax;
+		edi = eax+ebx+0x18;
+		*(LPDWORD)(DWORD)md5+0x14 = ecx;
+		if (ebp >= ecx)
+		{
+			eax = ecx;
+			ecx >>= 2;
+			for (int i=0; i<ecx; i++)
+				*(LPDWORD)(DWORD)edi+i = *(LPDWORD)(DWORD)esi+i; //+452D
+			ecx = eax;
+			ecx &= 0x3;
+			for (int i=0; i<ecx; i++)
+				*(LPBYTE)(DWORD)edi+i = *(LPBYTE)(DWORD)esi+i;
+			this->MakeDigest(ebx, ecx);
+			//TODO:
+		}
+		else
+		{
+		//+459B
+		}
 	}
 	else //(ecx == eax)
 	{
