25690fd8692d66de96e27344c10eadf6900b4e3e
umehkg <umehkg@users.noreply.github.com>
2014-12-20 00:53:38 +0800

    Create sub_69C600.cpp

---
+++ src/sp/sub_69C600.cpp
@@ -0,0 +1,7 @@
+int __thiscall sub_69C600(int param1, int param2, int param3)
+{
+  *(LPDWORD)(this + 0x9C) = param1;
+  *(LPDWORD)(this + 0xA0) = param2;
+  *(LPDWORD)(this + 0xA4) = param3;
+  return param3;
+}
