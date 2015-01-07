6ce006e80f0fe336f8423434d1de15d2bde0a879
umehkg <umehkg@users.noreply.github.com>
2015-01-07 18:25:38 +0800

    Update CPacket.h

--- server/CPacket.h
+++ server/CPacket.h
@@ -1,5 +1,5 @@
 /*
-Version 20141229
+Version 20150107
 
 */
 class CPacket
@@ -7,18 +7,24 @@ class CPacket
 private: //encapsulation
         long m_len;
         unsigned int m_type;
+		size_t m_mlen; //dynamic memory size of m_payload
         unsigned char *m_payload;
         bool m_isEncrypted;
-        void getLoginInfo();
 		unsigned long makeDigest();
 public:
+		/* use for server send */
 		CPacket(unsigned long packetType);
-		CPacket(unsigned long packetType, const void *data, size_t dataSize); //use for server send
-        CPacket(const void *payload, size_t dataSize, bool isEncrypted); //use for server recv
+		CPacket(unsigned long packetType, size_t packetSize);
+		CPacket(unsigned long packetType, const void *data, size_t dataSize);
+		/* use for server recv */
+        CPacket(const void *payload, size_t packetSize, bool isEncrypted);
+		//
         ~CPacket();
+		//
 		size_t GetSize();
 		unsigned long GetType();
 		void AppendData(void *dataBuffer, size_t dataSize);
+		void UpdateDataAt(unsigned long packetOffset, void *dataBuffer, size_t dataSize);
         void Encrypt();
         void Decrypt();
 		bool CheckPacket();
