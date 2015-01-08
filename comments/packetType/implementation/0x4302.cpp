4507acc2eaa887baa188c30d35969ac781248397
umehkg <umehkg@users.noreply.github.com>
2015-01-08 19:28:00 +0800

    Update 0x4302.cpp

--- comments/packetType/implementation/0x4302.cpp
+++ comments/packetType/implementation/0x4302.cpp
@@ -1,4 +1,4 @@
-//declarations
+	//declarations
 unsigned char myData1[0x978];
 char *myData = (char *)calloc(0x978-0x14, 0x1);
 long playerCardCount = 2;
@@ -8,23 +8,23 @@ long *playerCardAttribPtr2 = (long *)calloc(96, 4);
 long *playerCardAttribPtr3 = (long *)calloc(96, 4);
 //const
 const long minPointForLevelDword[29] = {-5L, 1L, 50L, 100L,
-                                    200L, 400L, 800L, 1600L,
-                                    2400L, 3200L, 6400L, 12800L,
-                                    25600L, 51200L, 102400L, 204800L,
-                                    409600L, 819200L, 1638400L, 3276800L,
-                                    6553600L, 13107200L, 26214400L, 52428800L,
-                                    104857600L, 209715200L, 419430400L, 838860800L,
-                                    1677721600L};
+									200L, 400L, 800L, 1600L,
+									2400L, 3200L, 6400L, 12800L,
+									25600L, 51200L, 102400L, 204800L,
+									409600L, 819200L, 1638400L, 3276800L,
+									6553600L, 13107200L, 26214400L, 52428800L,
+									104857600L, 209715200L, 419430400L, 838860800L,
+									1677721600L};
 const long long minPointForLevelQword[4] = {3355443200LL, 6710886400LL, 13421772800LL, 26843545600LL};
 //initial values
 long playerCardAttribArr0[2] = {5001, 1311};
 long playerCardAttribArr1[2] = {1, 2};
 long playerCardAttribArr2[2] = {3, 4};
 long playerCardAttribArr3[2] = {5, 6};
-memcpy(playerCardAttribPtr0, &playerCardAattribArr0[0], 2*4);
-memcpy(playerCardAttribPtr1, &playerCardAattribArr0[0], 2*4);
-memcpy(playerCardAttribPtr2, &playerCardAattribArr0[0], 2*4);
-memcpy(playerCardAttribPtr3, &playerCardAattribArr0[0], 2*4);
+memcpy(playerCardAttribPtr0, &playerCardAttribArr0[0], 2*4);
+memcpy(playerCardAttribPtr1, &playerCardAttribArr1[0], 2*4);
+memcpy(playerCardAttribPtr2, &playerCardAttribArr2[0], 2*4);
+memcpy(playerCardAttribPtr3, &playerCardAttribArr3[0], 2*4);
 
 
 char guildName[13] = "HKGolden";
@@ -35,7 +35,7 @@ long playerLevel = 0;
 long playerInventorySlots = 12;
 long playerType = 1; //set to 7 for GM...
 long whiteCards[4] = {0, 0, 0, 0};
-long scrolls[3] = {0, 0, 0}
+long scrolls[3] = {0, 0, 0};
 long playerWins = 0;
 long playerLoses = 0;
 long playerKOs = 0;
@@ -82,15 +82,14 @@ memcpy(myData+0x98-0x14, &whiteCards[3], 4); //wind
 memcpy(myData+0x9C-0x14, &channelFlag, 4); //enterable channel flag
 memcpy(myData+0xA0-0x14, &playerCardCount, 4); //param1 of call sub_473430
 
-memcpy(myData+0x280-0x14, playerCardAttrib0Ptr, 4*96); //param2 of call sub_473430
-memcpy(myData+0x400-0x14, playerCardAttrib1Ptr, 4*96); //param3 of call sub_473430
-memcpy(myData+0x580-0x14, playerCardAttrib2Ptr, 4*96); //param4 of call sub_473430
-memcpy(myData+0x700-0x14, playerCardAttrib3Ptr, 4*96); //param5 of call sub_473430
+memcpy(myData+0x280-0x14, playerCardAttribPtr0, 4*96); //param2 of call sub_473430
+memcpy(myData+0x400-0x14, playerCardAttribPtr1, 4*96); //param3 of call sub_473430
+memcpy(myData+0x580-0x14, playerCardAttribPtr2, 4*96); //param4 of call sub_473430
+memcpy(myData+0x700-0x14, playerCardAttribPtr3, 4*96); //param5 of call sub_473430
 
 memcpy(myData+0x880-0x14, &playerInventorySlots, 4);
 
-for (int i = 0; i < 29; i++)
-  memcpy(myData+0x884+i*8-0x14, &minPointForLevelDword[i], 4);
+memcpy(myData+0x884-0x14, &minPointForLevelDword[0], 4*29);
 memcpy(myData+0x8F8-0x14, &minPointForLevelQword[0], 4*8);
 
 memcpy(myData+0x94C-0x14, &lobbyMaxRooms, 4);
