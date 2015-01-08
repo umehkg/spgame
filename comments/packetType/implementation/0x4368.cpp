abd74d3891cdf8c1df5ad5622ae5c00b34fb4b31
umehkg <umehkg@users.noreply.github.com>
2015-01-09 00:48:15 +0800

    Update 0x4368.cpp

--- comments/packetType/implementation/0x4368.cpp
+++ comments/packetType/implementation/0x4368.cpp
@@ -1,32 +1,26 @@
 /* sendPacket 0x4368 - response to packetType 0x4367 */
-
-unsigned char myData1[0x698];
+unsigned char myData[0x698];
 char *myData = (char *)calloc(0x698-0x14, 0x1);
-
 unsigned long shopResult = 0;
-
 long long playerCode = 123000;
 long long playerCash = 456789;
 long long playerAvatarCoin = 2015;
-
 long playerSlotCount = 12;
 unsigned char inventoryItemExist[4] = {1, 1, 1, 1};
 unsigned char inventoryItemId[4] = {1111, 1112, 1113, 1114};
 unsigned char inventoryItemDays[4] = {0, 30, 90, 365};
 unsigned char inventoryItemLevelIdx[4] = {0, 4, 7, 8};
 unsigned char inventoryItemSk2Type[4] = {0, 400, 401, 402};
-
-memcpy(myData1+0x14-0x14, &shopResult, 4);
-memcpy(myData1+0x18-0x14, &inventoryItemExist[0], sizeof(inventoryItemExist));
-memcpy(myData1+0x78-0x14, &inventoryItemId[0], sizeof(inventoryItemId));
-memcpy(myData1+0x1F8-0x14, &inventoryItemDays[0], sizeof(inventoryItemDays));
-memcpy(myData1+0x378-0x14, &inventoryItemLevelIdx[0], sizeof(inventoryItemLevelIdx));
-memcpy(myData1+0x4F8-0x14, &inventoryItemSk2Type[0], sizeof(inventoryItemSk2Type));
-memcpy(myData1+0x678-0x14, &playerSlotCount, 4);
-memcpy(myData1+0x680-0x14, &playerCode, 8);
-memcpy(myData1+0x688-0x14, &playerCash, 4);
-memcpy(myData1+0x680-0x14, &playerAvatarCoin, 8);
-
+memcpy(myData+0x14-0x14, &shopResult, 4);
+memcpy(myData+0x18-0x14, &inventoryItemExist[0], sizeof(inventoryItemExist));
+memcpy(myData+0x78-0x14, &inventoryItemId[0], sizeof(inventoryItemId));
+memcpy(myData+0x1F8-0x14, &inventoryItemDays[0], sizeof(inventoryItemDays));
+memcpy(myData+0x378-0x14, &inventoryItemLevelIdx[0], sizeof(inventoryItemLevelIdx));
+memcpy(myData+0x4F8-0x14, &inventoryItemSk2Type[0], sizeof(inventoryItemSk2Type));
+memcpy(myData+0x678-0x14, &playerSlotCount, 4);
+memcpy(myData+0x680-0x14, &playerCode, 8);
+memcpy(myData+0x688-0x14, &playerCash, 4);
+memcpy(myData+0x680-0x14, &playerAvatarCoin, 8);
 CPacket *myPacket = new CPacket(0x4368, myData, 0x698-0x14);
 myPacket->Encrypt();
 myPacket->Output((char*)myData1, 0x978);
