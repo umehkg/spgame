459ef88f986b1ccf4ac5a99b2d6a307d06f4464f
umehkg <umehkg@users.noreply.github.com>
2015-01-08 21:12:39 +0800

    Update 0x4303.cpp

--- comments/packetType/implementation/0x4303.cpp
+++ comments/packetType/implementation/0x4303.cpp
@@ -1,26 +1,17 @@
-	unsigned char myData1[0x3C];
-	char *myData = (char *)calloc(0x3C-0x14, 0x1);
+			unsigned char myData1[0x3C];
+			char *myData = (char *)calloc(0x3C-0x14, 0x1);
+			char name[16] = "alanlei";
 
-  char unk1[16] = "";
-  long param1 = 0;
-  long param2 = 0;
-  long param3 = 0;
-  unsigned char param4 = 0;
-  unsigned char unk2 = 0; //0 or 1
-  long param5 = 0;
-  long param6 = 0;
-  
-	//buildData
-	memcpy(myData+0x14-0x14, &unk1[0], 16);
-	memcpy(myData+0x24-0x14, &param1, 4);
-	memcpy(myData+0x28-0x14, &param2, 4);
-	memcpy(myData+0x2C-0x14, &param3, 4);
-	memcpy(myData+0x30-0x14, &param4, 1);
-	memcpy(myData+0x31-0x14, &unk2, 1);
-	memcpy(myData+0x34-0x14, &param5, 4);
-	memcpy(myData+0x38-0x14, &param6, 4);	
-	//
-	CPacket *myPacket = new CPacket(0x4303, myData, 0x3C-0x14);
-	free(myData);
-	myPacket->Encrypt();
-	myPacket->Output( (char*)myData1, 0x3C );
+			memcpy(myData+0x14-0x14, name, strlen(name)+1);//player username
+			memcpy(myData+0x24-0x14, new long(10), 4);//player level:0~32
+			memcpy(myData+0x28-0x14, new long(2), 4);//list player order
+			memcpy(myData+0x2C-0x14, new long(0), 4);//unknown
+			memcpy(myData+0x30-0x14, new long(1), 4);//gender:0(F),1(M)
+			memcpy(myData+0x31-0x14, new char(2), 1);//must be 2
+			memcpy(myData+0x34-0x14, new long(0), 4);//unknown
+			memcpy(myData+0x38-0x14, new long(3), 4);//m-Level
+			
+			CPacket *myPacket = new CPacket(0x4303, myData, 0x3C-0x14);
+			free(myData);
+			myPacket->Encrypt();
+			myPacket->Output( (char*)myData1, 0x3C );
