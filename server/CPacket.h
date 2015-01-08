58c06276132b893bd552550604998bbb6db13ba6
umehkg <umehkg@users.noreply.github.com>
2015-01-08 23:28:08 +0800

    Update CPacket.h

--- server/CPacket.h
+++ server/CPacket.h
@@ -1,22 +1,24 @@
 /*
-Version 20150107
+Version 20150108
 
 */
+#pragma once
 class CPacket
 {
-private: //encapsulation
+protected: //encapsulation
         long m_len;
         unsigned int m_type;
 		size_t m_mlen; //dynamic memory size of m_payload
         unsigned char *m_payload;
         bool m_isEncrypted;
-		unsigned long makeDigest();
 public:
+		//default constructor
+		CPacket();
 		/* use for server send */
 		CPacket(unsigned long packetType);
 		CPacket(unsigned long packetType, size_t packetSize);
 		CPacket(unsigned long packetType, const void *data, size_t dataSize);
-		/* use for server recv */
+		/* use for server send/recv */
         CPacket(const void *payload, size_t packetSize, bool isEncrypted);
 		//
         ~CPacket();
@@ -27,6 +29,7 @@ public:
 		void UpdateDataAt(unsigned long packetOffset, void *dataBuffer, size_t dataSize);
         void Encrypt();
         void Decrypt();
+		unsigned long MakeDigest();
 		bool CheckPacket();
         void Output(char *outBuffer); //output payload to buffer passed as argument
 		void Output(unsigned char * outBuffer);//since you really like unsigned :S
