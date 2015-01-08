a44a95dc376cddcba0d837802a440d015eff0f3b
umehkg <umehkg@users.noreply.github.com>
2015-01-08 23:29:39 +0800

    Create xRecvPacket.h

---
+++ server/xRecvPacket.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CPacket.h"
+#include <stdlib.h>
+class CRecvPacket : public CPacket
+{
+private:
+	bool m_isValid;
+public:
+	CRecvPacket(void *recvBuffer, size_t packetSize);
+	~CRecvPacket(void);
+	unsigned char *GetPPtr();
+};
