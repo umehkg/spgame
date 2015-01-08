b3001d579b37031ee3c26dca42f30943a34422ab
umehkg <umehkg@users.noreply.github.com>
2015-01-08 17:39:53 +0800

    Update 0x4312.cpp
    bugfix

--- comments/packetType/implementation/0x4312.cpp
+++ comments/packetType/implementation/0x4312.cpp
@@ -1,19 +1,19 @@
 	unsigned char myData1[0xCC];
 	char *myData = (char *)calloc(0xCC-0x14, 0x1);
 	//knowns
-  long playerLevelList[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-  unsigned char playerGenderList[8] = {0, 1, 0, 1, 0, 1, 0, 1};
-  unsigned char playerNameList[8][12] = {"playerOne",
-                                        "playerTwo",
-                                        "player3",
-                                        "p4RoomOwner",
-                                        "player5",
-                                        "player6",
-                                        "player7",
-                                        "player8"
-                                        };
-  long playerIdxList[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-  long roomOwnerIdx = 3;
+	  long playerLevelList[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+	  unsigned char playerGenderList[8] = {0, 1, 0, 1, 0, 1, 0, 1};
+	  unsigned char playerNameList[8][13] = {"playerOne",
+	                                        "playerTwo",
+	                                        "player3",
+	                                        "p4RoomOwner",
+	                                        "player5",
+	                                        "player6",
+	                                        "player7",
+	                                        "player8"
+	                                        };
+	  long playerIdxList[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+	  long roomOwnerIdx = 3;
 	//buildData
 	memcpy(myData+0x14-0x14, new long(0), 4);
 	memcpy(myData+0x18-0x14, &playerLevelList[0], 4*8);
