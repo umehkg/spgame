8c7e454c9f3315fd179e6f112c4ac07abdcf80f6
umehkg <umehkg@users.noreply.github.com>
2015-01-09 21:11:13 +0800

    Update 0x4439.cpp

--- comments/packetType/implementation/0x4439.cpp
+++ comments/packetType/implementation/0x4439.cpp
@@ -2,7 +2,7 @@ if( *(long*)&decryptBuff[4] == 0x4438)
 {
 	long buyItemId = *(long *)&decryptBuff[0x14];
 	long buyItemAmount = *(long *)&decryptBuff[0x18];
-	unsigned char myData1[0x698];
+	unsigned char myData1[0x30];
 	char *myData = (char *)calloc(0x30-0x14, 0x1);
 	long buyResult = 0;
 	long long playerCode = 20150109;
