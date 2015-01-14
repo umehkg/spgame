5ec457cb6a76a86eae04968be62c726881258fa3
umehkg <umehkg@users.noreply.github.com>
2015-01-14 23:21:11 +0800

    Create 0x4468.cpp

---
+++ comments/packetType/implementation/0x4468.cpp
@@ -0,0 +1,12 @@
+if ( *(long*) &decryptBuff[4] == 0x4467 )
+{
+	unsigned char *myData = new unsigned char[0x20-0x14];
+	*(long *)myData = 1;
+	*(long *)&myData[4] = *(long *)&decryptBuff[0x14];
+	*(long *)&myData[8] = 96;
+	CPacket *myPacket = new CPacket(0x4468, &myData, 0x20-0x14);
+	delete myData;
+	myPacket->Encrypt();
+	WriteComm1((char*)myPacket->Payload(), myPacket->GetSize(), dwTimeout);
+	delete myPacket;
+}
